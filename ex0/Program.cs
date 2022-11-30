d13832bf5b179fc93d7667726a14768b9a9a5dbd
InVeRnyako <inver.nyako@gmail.com>
2022-11-30 15:32:53 +0300

    first commit

---
+++ ex0/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int M = 5;
+
+CountPositiveNumbersInc(M, out string lineOfNumbers, out int count);
+Console.WriteLine($"{lineOfNumbers} -> {count}");
+
+
+static void CountPositiveNumbersInc(int totalNum, out string lineOfNumbers, out int count)
+{
+    int newNumber;
+    lineOfNumbers = "";
+    count = 0;
+
+    for (int i = 1; i <= totalNum; i++)
+    {
+        newNumber = GetIntFromUser($"Введите число ({i} из {totalNum}): ");
+        lineOfNumbers = $"{lineOfNumbers}, {newNumber}";
+        if (newNumber > 0) count++;
+    }
+    lineOfNumbers = lineOfNumbers.Remove(0,2); //Убираем лишнюю запятую и пробел в начале строки
+    
+    return;
+}
+
+static int GetIntFromUser(string userMsg)
+{
+    while(true)
+    {
+        Console.Write(userMsg);
+        if (int.TryParse(Console.ReadLine(), out int num))
+            return num;
+        
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
---
+++ ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * ((b2 - b1) / (k1 - k2)) + b1
+
+Console.WriteLine("Поиск пересечения прямых, заданных формулами: y = k1 * x + b1, y = k2 * x + b2.");
+
+double b1 = GetDoubleFromUser("Введите значение b1: ");
+double k1 = GetDoubleFromUser("Введите значение k1: ");
+double b2 = GetDoubleFromUser("Введите значение b2: ");
+double k2 = GetDoubleFromUser("Введите значение k2: ");
+
+if (k1 == k2)
+    Console.WriteLine("Прямые не имеют точек пересечения.");
+else
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({FindXofLineIntersection(k1,k2,b1,b2)}; {FindYofLineIntersection(k1,k2,b1,b2)})");
+
+
+static double FindXofLineIntersection(double k1, double k2, double b1, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+static double FindYofLineIntersection(double k1, double k2, double b1, double b2)
+{
+    double y = k1 * ((b2 - b1) / (k1- k2)) + b1;
+    return y;
+}
+
+static double GetDoubleFromUser(string userMsg) //TryParse
+{
+    while(true)
+    {
+        Console.Write(userMsg);
+        if (double.TryParse(Console.ReadLine(), out double num))
+            return num;
+        
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
